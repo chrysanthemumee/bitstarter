90ec524286badaf46873f87b48467ec28dab9969
chrysanthemumee <stephy.jxu@gmail.com>
2014-02-02 20:49:02 -0600

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
+var fs = require('fs');
+var buffer = fs.readFileSync('index.html');
+var webpage = buf.toString();
+
+
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  response.send(webpage);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
